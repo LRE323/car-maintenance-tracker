248fbfda27055b37ecf633623467e8648b42a468
Luis Ruiz <ruizl1209@gmail.com>
2025-09-06 00:42:20 -0500

    Renaming theme

--- app/src/main/java/com/example/car_maintenance_tracker/views/MainActivity.kt
+++ app/src/main/java/com/example/car_maintenance_tracker/views/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.car_maintenance_tracker.ui.theme.Car_maintenance_trackerTheme
+import com.example.car_maintenance_tracker.ui.theme.CarMaintenanceTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Car_maintenance_trackerTheme {
+            CarMaintenanceTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Car_maintenance_trackerTheme {
+    CarMaintenanceTrackerTheme {
         Greeting("Android")
     }
 }
