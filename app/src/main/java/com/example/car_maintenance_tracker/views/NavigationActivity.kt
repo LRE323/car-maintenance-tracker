8423fd1d0922a1bb5a970d0feb6b645d0362c80d
Luis Ruiz <ruizl1209@gmail.com>
2025-09-06 01:15:05 -0500

    Adding Buttons with onClick functionality

--- app/src/main/java/com/example/car_maintenance_tracker/views/NavigationActivity.kt
+++ app/src/main/java/com/example/car_maintenance_tracker/views/NavigationActivity.kt
@@ -1,17 +1,25 @@
 package com.example.car_maintenance_tracker.views
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.car_maintenance_tracker.ui.theme.CarMaintenanceTrackerTheme
 
 class NavigationActivity : ComponentActivity() {
@@ -21,13 +29,57 @@ class NavigationActivity : ComponentActivity() {
         setContent {
             CarMaintenanceTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavigationScreen(innerPadding, getNavigationScreenOnClicks())
                 }
             }
         }
+    }
+
+    private fun getNavigationScreenOnClicks(): NavigationScreenOnClicks {
+        return NavigationScreenOnClicks(
+            onClickViewVehicles = { onClickButtonToast("Clicked View Vehicles") },
+            onClickLogItem = { onClickButtonToast("Clicked Log Item") },
+            onClickCreateNewVehicle = { onClickButtonToast("Clicked Create New Vehicle") }
+        )
+    }
+
+    private fun onClickButtonToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
+
+data class NavigationScreenOnClicks(
+    val onClickCreateNewVehicle: () -> Unit,
+    val onClickViewVehicles: () -> Unit,
+    val onClickLogItem: () -> Unit
+)
+
+@Composable
+private fun NavigationScreen(
+    innerPadding: PaddingValues,
+    navigationScreenOnClicks: NavigationScreenOnClicks
+) {
+    val modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding)
+        .padding(horizontal = 16.dp)
+        .background(color = Color.Red)
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NavigationOptions(navigationScreenOnClicks)
+    }
+}
+
+@Composable
+fun NavigationOptions(onClicks: NavigationScreenOnClicks) {
+    onClicks.apply {
+        NavigationButton("Create New Vehicle", onClickCreateNewVehicle)
+        NavigationButton("View Vehicles", onClickViewVehicles)
+        NavigationButton("Log Item", onClickLogItem)
     }
 }
 
