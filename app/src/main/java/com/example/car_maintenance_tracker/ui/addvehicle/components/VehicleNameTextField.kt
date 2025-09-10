cf3e500f4b7a7860a956083278439e08f5b0fd9e
Luis Ruiz <ruizl1209@gmail.com>
2025-09-09 22:13:12 -0500

    Fixing TextField not showing what user is inputting

--- app/src/main/java/com/example/car_maintenance_tracker/ui/addvehicle/components/VehicleNameTextField.kt
+++ app/src/main/java/com/example/car_maintenance_tracker/ui/addvehicle/components/VehicleNameTextField.kt
@@ -6,17 +6,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import com.example.car_maintenance_tracker.R
 
 @Composable
 fun VehicleMakeInput() {
-    val rememberInput by remember { mutableStateOf("") }
+    var rememberInput by remember { mutableStateOf("") }
     val labelText = stringResource(R.string.vehicle_make_label)
 
     TextField(
         value = rememberInput,
-        onValueChange = ::onVehicleMakeValueChange,
+        onValueChange = { rememberInput = onVehicleMakeValueChange(it) },
         label = { Text(text = labelText) },
         singleLine = true
     )
