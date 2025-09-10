c5498f64f882c37a66f7be2aaad8d6d35419cdbc
Luis Ruiz <ruizl1209@gmail.com>
2025-09-09 21:45:07 -0500

    Adding vehicle make input

--- app/src/main/java/com/example/car_maintenance_tracker/views/AddVehicleActivity.kt
+++ app/src/main/java/com/example/car_maintenance_tracker/views/AddVehicleActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,16 +12,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.car_maintenance_tracker.R
+import com.example.car_maintenance_tracker.ui.theme.CarMaintenanceTrackerTheme
 
 class AddVehicleActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldModifier = Modifier.fillMaxSize()
-            Scaffold(modifier = scaffoldModifier) { innerPadding ->
-                AddVehicleScreen(innerPadding, getAddVehicleScreenOnClicks())
+            CarMaintenanceTrackerTheme {
+                val scaffoldModifier = Modifier.fillMaxSize()
+                Scaffold(modifier = scaffoldModifier) { innerPadding ->
+                    AddVehicleScreen(innerPadding, getAddVehicleScreenOnClicks())
+                }
             }
         }
     }
@@ -47,7 +59,10 @@ fun AddVehicleScreen(
 
     Column(
         modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
+        VehicleMakeInput()
         AddVehicleButton(addVehicleScreenOnClicks.onClickAddVehicle)
     }
 }
@@ -59,6 +74,25 @@ fun AddVehicleButton(
     Button(onClickAddVehicle) {
         Text("Add Vehicle")
     }
+}
+
+@Composable
+fun VehicleMakeInput() {
+    var rememberInput by remember { mutableStateOf("") }
+    val labelText = stringResource(R.string.vehicle_make_label)
+
+    fun onValueChange(input: String) {
+        if (input.length <= 15) {
+            rememberInput = input
+        }
+    }
+
+    TextField(
+        value = rememberInput,
+        onValueChange = ::onValueChange,
+        label = { Text(text = labelText) },
+        singleLine = true
+        )
 }
 
 data class AddVehicleScreenOnClicks(val onClickAddVehicle: () -> Unit)
